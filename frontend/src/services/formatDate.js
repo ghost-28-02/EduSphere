18ff4ecb73d1b195c9b72cd819e2d7133ec36302
sachin <sachindhoor333@gmail.com>
2026-05-19 12:37:59 +0530

    style: instructor dashboard

--- frontend/src/services/formatDate.js
+++ frontend/src/services/formatDate.js
@@ -3,11 +3,11 @@ export const formatDate = (dateString) => {
     const date = new Date(dateString)
     const formatedDate = date.toLocaleDateString("en-US", options)
 
-    const hour = date.getHours()
+    let hour = date.getHours()
     const minutes = date.getMinutes()
     const period = hour >= 12 ? "PM" : "AM"
     hour = hour % 12 || 12;
-    const formatedTime = `${hour % 12}:${minutes.toString().padStart(2, "0")} ${period}`
+    const formatedTime = `${hour}:${minutes.toString().padStart(2, "0")} ${period}`
 
     return `${formatedDate} | ${formatedTime}`
 }
